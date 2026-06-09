272ffc7fe67b8ebf40a8015977eb9eb17b4fd4b0
Long Tran <tranvanlong6677@gmail.com>
2026-06-09 13:43:46 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { PostsModule } from './posts/posts.module';
+import { PostsModule } from './posts/posts.module';
+import { PostsModule } from './posts/posts.module';
+
+@Module({
+  imports: [PostsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
